e4d9d62e35ed3b131779a9f2f26119a622b541cb
sam-ratliff <sratliff@users.noreply.github.com>
2018-06-10 21:31:40 -0400

    Adding Zomato, jQuery

---
+++ ConQueso/www/scripts/zomato_api_credentials.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    // api key
+    //519f9322dea203f092b234d5b93a1b27
+    $("#getZomatoData").on("click", function () {
+        $.ajax({
+            method: "GET",
+            crossDomain: true,
+            url: "https://developers.zomato.com/api/v2.1/cities?q=Cincinnati",
+            dataType: "json",
+            async: true,
+            headers: {
+                "user-key": "519f9322dea203f092b234d5b93a1b27"
+            },
+            success: function (data) {
+                console.log("Data: " + data.location_suggestions[0].id);
+            },
+            error: function (x, y, z) {
+                console.log(x + ' ' + y + ' ' + z);
+            }
+        });
+    });
+});
